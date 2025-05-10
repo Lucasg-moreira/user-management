bef060fd5dba48e48b8a9e72401b9c0dfd199edb
Lucas Gabriel <lucasmoreira.gabriel2004@gmail.com>
2025-05-10 01:36:53 -0300

    rename

--- backend/src/main/java/com/github/lucasgms/usermanagement/features/auth/AuthController.java
+++ backend/src/main/java/com/github/lucasgms/usermanagement/features/auth/AuthController.java
@@ -1,23 +1,14 @@
 package com.github.lucasgms.usermanagement.features.auth;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.lucasgms.usermanagement.exception.BusinessException;
 import com.github.lucasgms.usermanagement.features.auth.dtos.RefreshTokenDto;
 import com.github.lucasgms.usermanagement.features.auth.dtos.TokenDto;
-import com.github.lucasgms.usermanagement.features.auth.dtos.UserLoginDTO;
+import com.github.lucasgms.usermanagement.features.auth.dtos.UserLoginDto;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
-import org.antlr.v4.runtime.Token;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Instant;
 import java.util.Map;
 
 @RequestMapping("/auth")
@@ -25,13 +16,12 @@ import java.util.Map;
 public class AuthController {
     private final AuthService service;
 
-
     AuthController(AuthService authService) {
         this.service = authService;
     }
 
     @PostMapping
-    public ResponseEntity<RefreshTokenDto> token (@RequestBody UserLoginDTO user, HttpServletResponse response) {
+    public ResponseEntity<RefreshTokenDto> token (@RequestBody UserLoginDto user, HttpServletResponse response) {
 
         var result = service.login(user);
 

dfb54303fbf6eb035e30b8220f3af1de69d51912
Lucas Gabriel <lucasmoreira.gabriel2004@gmail.com>
2025-05-10 01:40:19 -0300

    fix entity

--- backend/src/main/java/com/github/lucasgms/usermanagement/features/user/domain/entities/IndividualClient.java
+++ backend/src/main/java/com/github/lucasgms/usermanagement/features/user/domain/entities/IndividualClient.java
@@ -1,5 +1,7 @@
 package com.github.lucasgms.usermanagement.features.user.domain.entities;
 
+import com.github.lucasgms.usermanagement.features.user.domain.valueObject.Cpf;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 
 import java.time.LocalDate;
@@ -7,13 +9,15 @@ import java.time.LocalDate;
 @Entity
 public class IndividualClient extends Client {
     private String name;
-    private String cpf;
+    @Embedded
+    private Cpf cpf;
     private LocalDate birthDate;
 
-    public IndividualClient(String name, String cpf, LocalDate birthDate) {
+    public IndividualClient(String name, Cpf cpf, LocalDate birthDate, String telephone) {
         this.name = name;
         this.cpf = cpf;
         this.birthDate = birthDate;
+        this.setTelephone(telephone);
     }
 
     public String getName() {
@@ -24,11 +28,11 @@ public class IndividualClient extends Client {
         this.name = name;
     }
 
-    public String getCpf() {
+    public Cpf getCpf() {
         return cpf;
     }
 
-    public void setCpf(String cpf) {
+    public void setCpf(Cpf cpf) {
         this.cpf = cpf;
     }
 

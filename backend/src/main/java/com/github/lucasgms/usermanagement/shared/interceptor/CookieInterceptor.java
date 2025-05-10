4761815e12ff4c2c0b105cbfdb1c0848ae042b48
Lucas Gabriel <lucasmoreira.gabriel2004@gmail.com>
2025-05-10 01:46:10 -0300

    add CookieInterceptor

---
+++ backend/src/main/java/com/github/lucasgms/usermanagement/shared/interceptor/CookieInterceptor.java
@@ -0,0 +1,43 @@
+package com.github.lucasgms.usermanagement.shared.interceptor;
+
+import com.github.lucasgms.usermanagement.config.security.JWTConverter;
+import com.github.lucasgms.usermanagement.features.user.domain.entities.User;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class CookieInterceptor implements HandlerInterceptor {
+    JwtDecoder jwtDecoder;
+    JWTConverter jwtConverter;
+
+    public CookieInterceptor() {}
+    public CookieInterceptor(JwtDecoder jwtDecoder, JWTConverter jwtConverter) {
+        this.jwtDecoder = jwtDecoder;
+        this.jwtConverter = jwtConverter;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            Cookie[] cookies = request.getCookies();
+
+            for (Cookie cookie : cookies) {
+                if ("userToken".equals(cookie.getName())) {
+                    String token = cookie.getValue();
+                    Jwt jwt = jwtDecoder.decode(token);
+
+                    User userCookie = new User(jwt.getClaims().get("name").toString(),null, jwt.getClaims().get("sub").toString());
+                    request.setAttribute("user", userCookie);
+                    break;
+                }
+            }
+            return true;
+    }
+}
+
+
+

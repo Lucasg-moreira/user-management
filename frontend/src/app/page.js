b83eaa148520d12d0de7ed13bd4f0ef01c28486a
Lucas Gabriel <lucasmoreira.gabriel2004@gmail.com>
2025-05-12 19:14:27 -0300

    cleanup

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,26 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { getAccessToken, validateCookieDate } from "./components/clientSessionCheck";
-
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    let accessToken = getAccessToken()
-
-    if (!accessToken) {
-      router.push('/login')
-      return
-    }
-
-    if (validateCookieDate(accessToken))
-      router.push('/pessoas')
-    else
-      router.push('/login');
-
-  }, [router]);
-
   return null;
 }
